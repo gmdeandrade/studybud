f5313a891f307123ba0342ac081c37afe709035f
gmdeandrade <gmdeandradee1@gmail.com>
2024-03-23 21:06:01 -0300

    creating models

--- base/views.py
+++ base/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
-
-rooms = [
-    {"id": 1, "name": "Lets learn python!"},
-    {"id": 2, "name": "Study with me"},
-    {"id": 3, "name": "Frontend devs"},
-]
+from .models import Room
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {"rooms": rooms}
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
-    room = None
-    for r in rooms:
-        if r["id"] == int(pk):
-            room = r
+    room = Room.objects.get(id=pk)
     context = {"room": room}
     return render(request, "base/room.html", context)
